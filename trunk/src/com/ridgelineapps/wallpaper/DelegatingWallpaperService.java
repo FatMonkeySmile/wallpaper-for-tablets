0d509556447afad176d0b3a35d481d4d0bf5d705
fatmonkeysmile <fatmonkeysmile@dbd64782-0ea7-d710-7f13-9faaf36d62d7>
2011-12-29 23:41:55 +0000

    Removed unused imports

--- trunk/src/com/ridgelineapps/wallpaper/DelegatingWallpaperService.java
+++ trunk/src/com/ridgelineapps/wallpaper/DelegatingWallpaperService.java
@@ -30,9 +30,6 @@ import android.preference.PreferenceManager;
 import android.service.wallpaper.WallpaperService;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 import com.ridgelineapps.wallpaper.imagefile.ImageFileWallpaper;
